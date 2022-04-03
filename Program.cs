981ec0a52a5eb71e9f64a376477634021cd69a91
dute0987 <dute0987@gmail.com>
2022-04-03 18:41:22 +0530

    Greeting Message for AddressBook Problem using LINQ

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace AddressBookLinq
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressDataBook using LINQ !");
+            ////UC 2
+            //AdressDataBook adb = new AdressDataBook();
+            //adb.AddData();
+        }
+    }
+}
