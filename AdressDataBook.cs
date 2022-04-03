86d38ea67ad9f58b764182b97b5c8a6318f9e48b
dute0987 <dute0987@gmail.com>
2022-04-03 18:45:36 +0530

    Vishal Dute- data file for Createing TAble And Coloumn

---
+++ AdressDataBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookLinq
+{
+    public class AdressDataBook
+    {
+        public void AddData()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("FirstName");
+            table.Columns.Add("LastName");
+            table.Columns.Add("Address");
+            table.Columns.Add("City");
+            table.Columns.Add("State");
+            table.Columns.Add("Zip");
+            table.Columns.Add("PhoneNumber");
+            table.Columns.Add("Emails");
+
+        }
+    }
+}
